c52db24f5deed1aae3f35dcaecca9a94d59f1bb3
Brendan Long <brendan@arena.io>
2017-03-15 20:23:34 -0400

    Merge all the data in predict.py

--- predict.py
+++ predict.py
@@ -2,7 +2,6 @@
 import argparse
 import os
 
-import numpy as np
 import pandas as pd
 import tensorflow as tf
 
@@ -11,36 +10,34 @@ def load_csv(path, columns):
     this_dir = os.path.dirname(__file__)
     path = os.path.join(this_dir, path)
     df = pd.read_csv(path, usecols=list(columns))
-    df = df[~df.isnull().any(axis=1)]
     return df.apply(pd.to_numeric)
 
 
 def load_ncaa_games(year):
-    columns = {
-        "year": np.int,
-        "school_id": np.int,
-        "opponent_id": np.int,
-        "score": np.int,
-        "opponent_score": np.int
-    }
+    columns = ["year", "school_id", "opponent_id", "score", "opponent_score"]
     path = "csv/ncaa_games_%s.csv" % year
     return load_csv(path, columns)
 
 
 def load_ncaa_players(year):
-    columns = {
-        "school_id": np.int,
-        "player_id": np.int,
-        "points_avg": np.int
-    }
+    columns = ["school_id", "height", "points_avg"]
     path = "csv/ncaa_players_%s.csv" % year
-    return load_csv(path, columns)
+    players = load_csv(path, columns)
+    players = players[~players["height"].isnull()]
+    return players
 
 
 def load_data(year):
     games = load_ncaa_games(year)
     players = load_ncaa_players(year)
-    return pd.merge(games, players, how="inner", on="school_id")
+    other_team = players.rename(columns={
+        "school_id": "opponent_id",
+        "height": "opponent_height",
+        "points_avg": "opponent_points_avg"
+    })
+    games = pd.merge(games, players, how="inner", on="school_id")
+    games = pd.merge(games, other_team, how="inner", on="opponent_id")
+    return games
 
 
 if __name__ == "__main__":
@@ -56,12 +53,12 @@ if __name__ == "__main__":
 
     dfs = [load_data(year) for year in args.train_years + [args.predict_year]]
     df = pd.concat(dfs)
+    df["win"] = df["score"] > df["opponent_score"]
 
-    feature_cols = ["year", "school_id", "opponent_id"]
+    feature_cols = ["height", "opponent_height"]
 
     features = tf.contrib.learn.extract_pandas_data(df[feature_cols])
-    labels = tf.contrib.learn.extract_pandas_labels(
-        df["score"] > df["opponent_score"])
+    labels = tf.contrib.learn.extract_pandas_labels(df["win"])
 
     feature_columns = tf.contrib.learn.infer_real_valued_columns_from_input(
         features)
@@ -69,5 +66,5 @@ if __name__ == "__main__":
         feature_columns=feature_columns)
     train = df["year"] != args.predict_year
     estimator.fit(
-        x=features[train], y=labels[train], steps=10000, batch_size=100)
-    print(estimator.evaluate(x=features[~train], y=labels[~train]))
+        x=features[train], y=labels[train], steps=10, batch_size=100)
+    print(estimator.evaluate(x=features[~train][:100], y=labels[~train][:100]))
