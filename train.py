20d2429846492641d562d2e07276405d4cc46085
Brendan Long <brendan@arena.io>
2017-03-15 22:01:32 -0400

    Load the data as a multidimensional array.

--- train.py
+++ train.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 import argparse
+import functools
+import multiprocessing
 import os
 
+import numpy as np
 import pandas as pd
 import tensorflow as tf
+
+
+# All teams need to be the same size, so we pad them to this size
+MAX_PLAYERS = 56
 
 
 def load_csv(path, columns):
@@ -27,17 +34,50 @@ def load_ncaa_players(year):
     return players
 
 
+def load_game(players, p):
+    i, game = p
+    this_team = players[players["school_id"] == game["school_id"]]
+    this_team = this_team.as_matrix(columns=["height", "points_avg"])
+    other_team = players[players["school_id"] == game["opponent_id"]]
+    other_team = other_team.as_matrix(columns=["height", "points_avg"])
+
+    if len(this_team) == 0 or len(other_team) == 0:
+        return None, None
+    while len(this_team) < MAX_PLAYERS:
+        random_index = np.random.choice(this_team.shape[0])
+        random_player = this_team[random_index]
+        this_team = np.vstack([this_team, [random_player]])
+    while len(other_team) < MAX_PLAYERS:
+        random_index = np.random.choice(other_team.shape[0])
+        random_player = other_team[random_index]
+        other_team = np.vstack([other_team, [random_player]])
+    teams = [this_team, other_team]
+    if i % 1000 == 0:
+        print("Handled row %s" % i)
+    return np.stack(teams), [game["score"] > game["opponent_score"]]
+
+
 def load_data(year):
-    games = load_ncaa_games(year)
-    players = load_ncaa_players(year)
-    other_team = players.rename(columns={
-        "school_id": "opponent_id",
-        "height": "opponent_height",
-        "points_avg": "opponent_points_avg"
-    })
-    games = pd.merge(games, players, how="inner", on="school_id")
-    games = pd.merge(games, other_team, how="inner", on="opponent_id")
-    return games
+    with multiprocessing.Pool(16) as pool:
+        features_path = "data/features_%s.npy" % year
+        labels_path = "data/labels_%s.npy" % year
+        if not os.path.exists(features_path) \
+                or not os.path.exists(labels_path):
+            games = load_ncaa_games(year)
+            players = load_ncaa_players(year).fillna(0)
+            len_rows = games.shape[0]
+            print("Iterating through %s games" % len_rows)
+            f = functools.partial(load_game, players)
+            res = pool.map(f, games.iterrows())
+            features = [feature for feature, _ in res if feature is not None]
+            labels = [label for _, label in res if label is not None]
+            features = np.array(features, dtype=np.float32)
+            labels = np.array(labels, dtype=np.float32)
+            os.makedirs(os.path.dirname(features_path), exist_ok=True)
+            os.makedirs(os.path.dirname(labels_path), exist_ok=True)
+            np.save(features_path, features)
+            np.save(labels_path, labels)
+        return np.load(features_path), np.load(labels_path)
 
 
 if __name__ == "__main__":
@@ -51,20 +91,18 @@ if __name__ == "__main__":
     if args.verbose:
         tf.logging.set_verbosity(tf.logging.INFO)
 
-    dfs = [load_data(year) for year in args.train_years + [args.predict_year]]
-    df = pd.concat(dfs)
-    df["win"] = df["score"] > df["opponent_score"]
+    data = [load_data(year) for year in args.train_years]
+    features = np.vstack([features for features, _ in data])
+    labels = np.vstack([labels for _, labels in data])
+    assert len(features) == len(labels)
 
-    feature_cols = ["height", "opponent_height"]
+    feature_cols = \
+        tf.contrib.learn.infer_real_valued_columns_from_input(features)
 
-    features = tf.contrib.learn.extract_pandas_data(df[feature_cols])
-    labels = tf.contrib.learn.extract_pandas_labels(df["win"])
-
-    feature_columns = tf.contrib.learn.infer_real_valued_columns_from_input(
-        features)
     estimator = tf.contrib.learn.LinearClassifier(
-        feature_columns=feature_columns)
-    train = df["year"] != args.predict_year
+        feature_columns=feature_cols)
     estimator.fit(
-        x=features[train], y=labels[train], steps=10, batch_size=100)
-    print(estimator.evaluate(x=features[~train][:100], y=labels[~train][:100]))
+        x=features, y=labels, steps=10000, batch_size=1000)
+
+    test_features, test_labels = load_data(args.predict_year)
+    print(estimator.evaluate(x=test_features, y=test_labels))
