382293f097c95374fd4a37bc7432e69efcc10226
Brendan Long <brendan@getpoint.io>
2023-07-27 16:13:00 -0600

    Move venv to venv folder and load correctly in direnv

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-python3 -m venv .
-source bin/activate
+#!/bin/bash -e
+python3 -m venv venv
+source venv/bin/activate
 python -m pip install -r requirements.txt
 direnv allow 2>&1 > /dev/null || true
